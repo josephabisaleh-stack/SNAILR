8bc70fca635e36fea2566e2bf044b5e03ad80999
Jeremy Nizard <nizardjeremy@gmail.com>
2026-03-03 17:34:04 +0100

    objectives+chats+messages controllers + views (show + index)

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,42 +1,44 @@
 class ObjectivesController < ApplicationController
-  before_action :set_objective, only: [:show, :edit, :update, :destroy, :confirm]
+  before_action :set_objective, only: [:show] # :edit, :update, :destroy, :confirm
 
   def index
     @objectives = current_user.objectives
+    # A modifier later
   end
 
   def show
+    @chat = @objective.chat
+    @message = @chat.messages.new
   end
 
-  def new
-    @objective = Objective.new
-  end
+  # def new
+  #   @objective = Objective.new
+  # end
 
-  def create
-    @objective = current_user.objectives.build(objective_params)
-    if @objective.save
-      redirect_to objective_path(@objective), notice: "Objective created."
-    else
-      render :new, status: :unprocessable_entity
-    end
-  end
+  # def create
+  #   @objective = current_user.objectives.build(objective_params)
+  #   if @objective.save
+  #     redirect_to objective_path(@objective), notice: "Objective created."
+  #   else
+  #     render :new, status: :unprocessable_entity
+  #   end
+  # end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-    if @objective.update(objective_params)
-      redirect_to objective_path(@objective), notice: "Objective updated."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @objective.update(objective_params)
+  #     redirect_to objective_path(@objective), notice: "Objective updated."
+  #   else
+  #     render :edit, status: :unprocessable_entity
+  #   end
+  # end
 
-  def destroy
-    @objective.destroy
-    redirect_to objectives_path, notice: "Objective deleted."
-  end
-
+  # def destroy
+  #   @objective.destroy
+  #   redirect_to objectives_path, notice: "Objective deleted."
+  # end
 
   private
 
@@ -44,7 +46,7 @@ class ObjectivesController < ApplicationController
     @objective = current_user.objectives.find(params[:id])
   end
 
-  def objective_params
-    params.require(:objective).permit(:title, :description)
-  end
+  # def objective_params
+  #   params.require(:objective).permit(:title, :description)
+  # end
 end
